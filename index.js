00627ee40492eb0ffda06d98102774c8f38af201
Guilherme Bechuate <guiubech@hotmail.com>
2020-10-02 07:15:06 -0300

    Conversor funcionando

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 //Mudar de trabalho, trabalahra para mim como desenvolvedor
 //Criar uma rotina de estudo e desenvolvimento
+
+
+let binario = numero => {
+    let binarioArray = []
+    while(numero>1){
+        binarioArray.push(numero % 2 );
+        numero = Math.floor(numero / 2 );
+    }
+    binarioArray.push(numero);
+    return parseInt(binarioArray.reverse().join(""));
+}
+console.log(binario(100));    
